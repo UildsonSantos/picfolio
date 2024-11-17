b904722aa9a1f1076b9ad26fcefa325987065541
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-11-17 18:55:52 -0300

    Implement lazy loading for ImageGallery component to optimize initial load time.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { SearchProvider } from './context/SearchContext';
 import SearchBar from './components/SearchBar/SearchBar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
 
 import './App.css';
+
+// Lazy loading para ImageGallery
+const ImageGallery = lazy(() => import('./components/ImageGallery/ImageGallery'));
 
 function App() {
   return (
     <SearchProvider>
       <div className="App">
         <SearchBar />
-        <ImageGallery />
+        {/* Suspense para exibir fallback durante o carregamento */}
+        <Suspense fallback={<div>Loading gallery...</div>}>
+          <ImageGallery />
+        </Suspense>
       </div>
     </SearchProvider>
   );
