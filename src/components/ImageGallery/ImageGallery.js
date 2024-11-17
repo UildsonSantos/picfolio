728d34c2ab199e377808872df032d1b9759757f9
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-11-17 19:48:54 -0300

    feat: add modal functionality to display images in fullscreen

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { SearchContext } from '../../context/SearchContext';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -16,37 +16,62 @@ import './styles.css';
 
 const ImageGallery = () => {
   const { images, isLoading, imagesNotFoundError } = useContext(SearchContext);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  const closeModal = () => {
+    setSelectedImage(null);
+  };
 
   return (
     <div className="gallery">
       {isLoading && <LoadingSpinner />}
       {imagesNotFoundError && <p style={{ color: 'red' }}>{imagesNotFoundError}</p>}
       {!isLoading && images.length > 0 ? (
-        <Swiper
-          spaceBetween={20}
-          slidesPerView={1}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-            1400: {
-              slidesPerView: 4,
-              spaceBetween: 50,
-            },
-          }}
-          modules={[Pagination]}
-          className="gallery-swiper"
-        >
-          {images.map((image) => (
-            <SwiperSlide key={image.id} watchSlidesProgress={true}>
-              <img src={image.urls.small} alt={image.alt_description} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <>
+          <Swiper
+            spaceBetween={20}
+            slidesPerView={1}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 40
+              },
+              1400: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Pagination]}
+            className="gallery-swiper"
+          >
+            {images.map((image) => (
+              <SwiperSlide key={image.id} watchSlidesProgress={true} onClick={() => handleImageClick(image)}>
+                <img src={image.urls.small} alt={image.alt_description} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {selectedImage && (
+            <div className="modal" onClick={closeModal}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <img
+                  src={selectedImage.urls.full}
+                  alt={selectedImage.alt_description}
+                  style={{ width: '100%', borderRadius: '8px' }}
+                />
+                <button className="close-button" onClick={closeModal}>
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
+        </>
       ) : (
         <p>Use the search above to find images</p>
       )}
